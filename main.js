f3b6f87f280c1ae01220750a242cbc4fc40e7656
emma jamieson <emma.cody@hotmail.co.uk>
2023-02-15 08:55:09 +0000

    updated CSS and JS for carousel image

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-$('.carousel').carousel({
-    interval: 2000
-  })
+$(document).ready(function(){
+  console.log($('#carouselwithIndicators').css('height')); // check the initial height of the carousel;
+  
+  // now apply this height as a max-height on all the image items; css will handle the rest;
+  $('#carouselwithIndicators').find('.carousel-item img').css('max-height', $('#carouselwithIndicators').css('height'))
+});
